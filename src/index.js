b057efccdea61882a93c7398f0abf7c7301488a4
cqrlosArch <diaz.flores.c@gmail.com>
2021-07-09 19:39:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//scss
+import './scss/styles.scss';
+//images
+import './assets/images/img.jpg';
+import './assets/images/code.png';
+import './assets/images/dog.jpg';
+import './assets/images/favicons/apple-touch-icon180.png';
+import './assets/images/favicons/favicon_16x16.png';
+import './assets/images/favicons/favicon.ico';
+//vendors
+import './vendors/jquery-3.5.1.min.js';
+
+//Modules
+import getData from './common/module';
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+getData().then((data) => console.log(data));
+
+console.log('Hello!!!');
---
+++ webpack/webpack.common.js
@@ -0,0 +1,149 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+// const { ESBuildPlugin } = require('esbuild-loader')
+const path = require('path');
+const fs = require('fs');
+
+const PAGES_DIR = path.resolve(__dirname, '..', 'src' + '/views/pages/');
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith('.pug'));
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, '..', './dist'),
+    filename: 'js/[name].[contenthash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/images',
+              useRelativePath: true,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff2?)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/fonts',
+              publicPath: '../assets/fonts',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|mp3|txt|xml|pdf|json)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/data',
+              publicPath: '../assets/data',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '..', './dist'),
+  },
+  optimization: {
+    nodeEnv: 'production',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors', // part of the bundle name and
+          // can be used in chunks array of HtmlWebpackPlugin
+          test: /[\\/](node_modules|vendors)[\\/]/,
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: ['*', '.js', '.css', '.scss', '.pug', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/views/index.pug",
+    }),
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./pages/${page.replace(/\.pug/, '.html')}`,
+          // inject: false,
+        })
+    ),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    }),
+    new ESLintPlugin(),
+  ],
+};
